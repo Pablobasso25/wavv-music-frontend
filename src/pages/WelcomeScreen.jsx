63eb81acd93c9a2bede6dda67f2f24c26f0ea2d5
Patricio Manuel Romero Celiz <patorceliz05@gmail.com>
2025-11-26 20:47:07 -0300

    style: ajustar colores del reproductor

--- src/pages/WelcomeScreen.jsx
+++ src/pages/WelcomeScreen.jsx
@@ -1,32 +1,27 @@
-
-
+import { useFormState } from "react";
+import {Container, Row, Col, Spinner} from "react-bootstrap";
 const WelcomeScreen = () => {
 return (
     <Container
       fluid
       className="vh-100 d-flex align-items-center justify-content-center"
      style={{
-        backgroundColor
+        backgroundColor: "#000"
+        background: "linear-gradient(135deg, #000000 0%, #1a1a1a 100%)",
      }}
     >
       <Row className="align-items-center text-white text-center">
         <Col>
           <h1
             className="text-primary mb-4"
-            style={{ fontSize: "3.5rem", fontWeight: "bold" }}
+           style={{ fontSize:"3.5rem", fontWeight: "bold"}}
           >
             Wavv
           </h1>
           <div className="mb-4">
             <h3 className="text-secondary mb-3">Sintonizando...</h3>
-            <Spinner
-              animation="grow"
-              variant="primary"
-              style={{ width: "3rem", height: "3rem" }}
-            />
+           
           </div>
-
-          {/* Barra de progreso opcional */}
           <div style={{ width: "300px", margin: "0 auto" }}>
             <div
               style={{
