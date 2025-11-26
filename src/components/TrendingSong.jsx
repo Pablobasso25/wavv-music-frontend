94e263fc9412ad8ba864579a863fefba2c857530
RominaDanelutto <r.danelutto@gmail.com>
2025-11-26 18:26:30 -0300

    feat: agrega imports de bootstrap, assets y context para TrendingSong

--- src/components/TrendingSong.jsx
+++ src/components/TrendingSong.jsx
@@ -1,4 +1,7 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { Container, Row, Col, Button } from "react-bootstrap";
+import trendImg from "../assets/trend.png";
+import { useMusicPlayer } from "../context/MusicPlayerContext";
 
 const TrendingSong = () => {
   return (
