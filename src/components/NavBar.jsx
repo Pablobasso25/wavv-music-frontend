709228aadd09ab440550ed0bab4803c7ce57c55c
AlvaroMorillo <alvaromorillo28@gmail.com>
2025-11-26 16:08:39 -0300

    feat: agregar enlaces de navegación entre páginas home y playlist en componente navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,39 @@
-import React from 'react'
+import { Navbar, Container, Nav, Form, Dropdown, Badge } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <Navbar expand="lg" className="py-3" style={{ backgroundColor: "#000" }}>
+      <Container>
+        <Navbar.Brand href="#" className="text-white fw-bold logo-custom">
+          Wavv
+        </Navbar.Brand>
+        <Navbar.Toggle
+          aria-controls="basic-navbar-nav"
+          className="text-secondary border-0"
+        />
 
-export default NavBar
+        <NavBar.Collapse id="basic-navbar-nav">
+          <Nav className="mx-auto my-3 my-lg-0 text-center">
+            <NavLink
+              to="/"
+              className="text-secondary text-uppercase fw-bold mx-2 nav-link-custom"
+            >
+              <i className="bx bx-home-alt me-1"></i>
+              Home
+            </NavLink>
+            <NavLink
+              to="/playlist"
+              className="text-secondary text-uppercase fw-bold mx-2 nav-link-custom"
+            >
+              <i className="bx bx-list-ul me-1"></i>
+              Playlist
+            </NavLink>
+          </Nav>
+        </NavBar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default NavBar;
