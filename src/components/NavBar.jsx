7795cb0b60e644ad4c9ae3459e47a03800b43d96
AlvaroMorillo <alvaromorillo28@gmail.com>
2025-11-26 21:06:38 -0300

    feat: agregar enlaces de navegación entre páginas nosotros y admin en componente navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,33 +3,36 @@ import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
   return (
-    <Navbar expand="lg" className="py-3" style={{ backgroundColor: "#000" }}>
+    <Navbar>
       <Container>
-        <Navbar.Brand href="#" className="text-white fw-bold logo-custom">
+        <Navbar.Brand>
           Wavv
         </Navbar.Brand>
-        <Navbar.Toggle
-          aria-controls="basic-navbar-nav"
-          className="text-secondary border-0"
-        />
 
-        <NavBar.Collapse id="basic-navbar-nav">
-          <Nav className="mx-auto my-3 my-lg-0 text-center">
-            <NavLink
-              to="/"
-              className="text-secondary text-uppercase fw-bold mx-2 nav-link-custom"
-            >
-              <i className="bx bx-home-alt me-1"></i>
+        <Navbar.Toggle/>
+
+        <NavBar.Collapse>
+          <Nav>
+            <NavLink>
               Home
             </NavLink>
-            <NavLink
-              to="/playlist"
-              className="text-secondary text-uppercase fw-bold mx-2 nav-link-custom"
-            >
-              <i className="bx bx-list-ul me-1"></i>
+            <NavLink>
               Playlist
             </NavLink>
+            <NavLink>
+              NOSOTROS
+            </NavLink>
+            <NavLink>
+              ADMIN
+            </NavLink>
           </Nav>
+          <div>
+            <div>
+              <div>
+                <span>Buscando...</span>
+              </div>
+            </div>
+          </div>
         </NavBar.Collapse>
       </Container>
     </Navbar>
