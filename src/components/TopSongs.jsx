f8ef987274b31b5150524be7e9659544f09c4418
RominaDanelutto <r.danelutto@gmail.com>
2025-11-27 17:29:01 -0300

    feat(topSongs): define componente TopSongs y estados básicos

--- src/components/TopSongs.jsx
+++ src/components/TopSongs.jsx
@@ -2,3 +2,7 @@ import { Container } from "react-bootstrap";
 import { useState, useEffect } from "react";
 import trendImg from "../assets/trend.png";
 import { useMusicPlayer } from "../context/MusicPlayerContext";
+
+const TopSongs = ({ album, isPlaylist = false, fromHome = false }) => {
+  const { playSong, currentSong, isPlaying } = useMusicPlayer();
+  const [playlist, setPlaylist] = useState([]);
