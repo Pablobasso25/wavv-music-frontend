8a8711f32bc1f59d67d6d65b2762cf622591d017
RominaDanelutto <r.danelutto@gmail.com>
2025-11-27 17:30:22 -0300

    feat(topSongs): implementa carga de playlist y listeners de actualización

--- src/components/TopSongs.jsx
+++ src/components/TopSongs.jsx
@@ -6,3 +6,22 @@ import { useMusicPlayer } from "../context/MusicPlayerContext";
 const TopSongs = ({ album, isPlaylist = false, fromHome = false }) => {
   const { playSong, currentSong, isPlaying } = useMusicPlayer();
   const [playlist, setPlaylist] = useState([]);
+
+    useEffect(() => {
+    const loadPlaylist = () => {
+      const storedPlaylist =
+        JSON.parse(localStorage.getItem("userPlaylist")) || [];
+      setPlaylist(storedPlaylist);
+    };
+
+    loadPlaylist();
+
+    window.addEventListener("storage", loadPlaylist);
+    const customListener = () => loadPlaylist();
+    window.addEventListener("playlistUpdated", customListener);
+
+    return () => {
+      window.removeEventListener("storage", loadPlaylist);
+      window.removeEventListener("playlistUpdated", customListener);
+    };
+  }, []);
