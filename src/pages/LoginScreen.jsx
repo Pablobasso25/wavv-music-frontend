b8748bbeb3d4c9489a47491934cc0a7e268fc943
Patricio Manuel Romero Celiz <patorceliz05@gmail.com>
2025-11-30 13:08:59 -0300

    feat: agrega componentes visuales, hooks, navegacion, contexto de autentiacion

---
+++ src/pages/LoginScreen.jsx
@@ -0,0 +1,12 @@
+import {
+  Container,
+  Row,
+  Col,
+  Card,
+  Form,
+  Button,
+  Alert,
+} from "react-bootstrap";
+import { useState } from "react";
+import { useNavigate, Link } from "react-router-dom";
+import { useAuth } from "../../context/AuthContext";
