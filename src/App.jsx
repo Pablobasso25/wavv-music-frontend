d51d537a5addb5e0a76695a5232946f0af8d4137
Pablo Basso <pablobasso25@gmail.com>
2025-11-30 00:29:11 -0300

    feat: Añadir pantalla de bienvenida con temporizador de carga

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useState, useEffect } from "react";
 import RegisterScreen from "./pages/Autenticacion/RegisterScreen";
 import LoginScreen from "./pages/Autenticacion/LoginScreen";
-
+import WelcomeScreen from "./pages/WelcomeScreen";
 const App = () => {
+  const [welcome, setWelcome] = useState(true);
+
+  useEffect(() => {
+    const cargando = setTimeout(() => setWelcome(false), 3000);
+    return () => clearTimeout(cargando);
+  }, []);
+
+  if (welcome) return <WelcomeScreen />;
   return (
     <Router>
       <Routes>
