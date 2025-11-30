e13ca884c2f8b18a8bb9203580965908536854b7
Pablo Basso <pablobasso25@gmail.com>
2025-11-30 00:31:13 -0300

    feat: Implementar ruta protegida para Home con NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,16 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { useState, useEffect } from "react";
 import RegisterScreen from "./pages/Autenticacion/RegisterScreen";
 import LoginScreen from "./pages/Autenticacion/LoginScreen";
+import ProtectedRoute from "./routes/ProtectedRoute";
+import HomeScreen from "./pages/Home/HomeScreen";
+import NavBar from "./components/NavBar";
 import WelcomeScreen from "./pages/WelcomeScreen";
 const App = () => {
   const [welcome, setWelcome] = useState(true);
 
   useEffect(() => {
-    const cargando = setTimeout(() => setWelcome(false), 3000);
-    return () => clearTimeout(cargando);
+    const loading = setTimeout(() => setWelcome(false), 3000);
+    return () => clearTimeout(loading);
   }, []);
 
   if (welcome) return <WelcomeScreen />;
@@ -17,6 +20,15 @@ const App = () => {
       <Routes>
         <Route path="/login" element={<LoginScreen />} />
         <Route path="/register" element={<RegisterScreen />} />
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <NavBar />
+              <HomeScreen />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </Router>
   );
