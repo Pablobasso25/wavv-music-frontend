c62a77400b4a1f0a96631610bb477c667cf3b973
Juan Ferreyra <juanitouu.f@gmail.com>
2025-12-01 18:15:35 -0300

    feat: agrega footer de la pagina

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,51 @@
-import React from 'react'
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import { FaGithub, FaInstagram } from "react-icons/fa";
+import logo from "./logo.jpeg";
 
 const Footer = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <footer style={{ background: "black", color: "white", padding: "40px" }}>
+      <Container>
+        <Row className="align-items-center text-center text-md-start">
 
-export default Footer
+          <Col md={4}>
+            <img
+              src={logo}
+              alt="Logo"
+              style={{ height: "90px", objectFit: "contain" }}
+            />
+          </Col>
+
+          <Col
+            md={4}
+            className="d-flex justify-content-center gap-5 mt-3 mt-md-0"
+          >
+            <a href="#" style={{ color: "white", textDecoration: "none" }}>
+              Inicio
+            </a>
+            <a href="#" style={{ color: "white", textDecoration: "none" }}>
+              Nosotros
+            </a>
+          </Col>
+
+          <Col
+            md={4}
+            className="d-flex justify-content-center justify-content-md-end gap-3 mt-3 mt-md-0"
+            style={{ fontSize: "28px" }}
+          >
+            <a href="#" style={{ color: "#b56dff" }}>
+              <FaGithub />
+            </a>
+            <a href="#" style={{ color: "#b56dff" }}>
+              <FaInstagram />
+            </a>
+          </Col>
+
+        </Row>
+      </Container>
+    </footer>
+  );
+};
+
+export default Footer;
