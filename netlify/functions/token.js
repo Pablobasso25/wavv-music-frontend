c2a07a187c95be31ce71bd882bacf2d1c65bf3ea
Pablo Basso <pablobasso25@gmail.com>
2025-12-03 15:57:12 -0300

    fix: corregir import de node-fetch en Netlify Functions

--- netlify/functions/token.js
+++ netlify/functions/token.js
@@ -1,4 +1,5 @@
-import fetch from "node-fetch";
+const fetch = (...args) =>
+  import("node-fetch").then(({ default: fetch }) => fetch(...args));
 
 export async function handler(event, context) {
   try {
