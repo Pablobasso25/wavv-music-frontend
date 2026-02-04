05479a643c1480f69468cb495c1e4f6f1d8bd8d5
Pablo Basso <pablobasso25@gmail.com>
2026-02-04 17:03:18 -0300

    [WM-1] feat: Establece conexion con el backend y las rutas de peticiones

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,5 @@
 import axios from "axios";
 
-// Verificamos si estamos en producción o desarrollo
 const API_URL = import.meta.env.VITE_API_URL || "http://localhost:4000/api";
 
 const instance = axios.create({
