c42d05ee2eb0246aa1cf2570dc1427bb963543a5
Pablo Basso <pablobasso25@gmail.com>
2026-02-05 18:16:42 -0300

    [WM-15]feat: agrega rutas de para las peticiones al backend a travez de axios

---
+++ src/api/songs.js
@@ -0,0 +1,9 @@
+import axios from "./axios";
+
+export const getSongsRequest = () => axios.get("/songs");
+export const createSongRequest = (song) => axios.post("/songs", song);
+export const deleteSongRequest = (id) => axios.delete(`/songs/${id}`);
+export const getSongRequest = (id) => axios.get(`/songs/${id}`);
+export const searchExternalSongsRequest = (term) =>
+  axios.get(`/search-external?search=${term}`);
+export const addSongToPlaylistRequest = (songId) => axios.post("/playlist/add", { songId });
