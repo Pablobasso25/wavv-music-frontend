ef84ca7fa252d77cc40b954617051fa4d7e6ad5e
Juan Ferreyra <juanitouu.f@gmail.com>
2026-02-11 19:20:35 -0300

    [WM-30] feat: agrega envio de notificacion generica de EmailJS

--- src/services/emailService.js
+++ src/services/emailService.js
@@ -13,4 +13,27 @@ export const sendWelcomeEmail = async (username, email) => {
         from_name: "Wavv Music",
         date: new Date().toLocaleDateString(),
     }
+    try {
+     const response = await emailjs.send(
+        SERVICE_ID,
+        TEMPLATE_ID,
+        templateParams,
+     );
+     return response;
+    } catch (error) {
+    console.error("Error en EmailJS:", error);
+    throw error;
+    }
+};
+const TEMPLATE_GENERICO = import.meta.env.VITE_EMAILJS_TEMPLATE_GENERICO;
+
+const sendNotification = async (data) => {
+    const params = {
+        to_name: data.username,
+        to_email: data.email,
+        dynamic_issue: data.dynamic_issue,
+        body_message: data.body_message,
+        from_name: "Wavv Music",
+    }
+    return await emailjs.send(SERVICE_ID, TEMPLATE_GENERICO, params);
 }
