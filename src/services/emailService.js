a3f0d4ae8890216a683b7c73269f77924b4d9679
Juan Ferreyra <juanitouu.f@gmail.com>
2026-02-11 18:49:30 -0300

    [WM-30] feat: agrega funcionalidad de envio de email cuando el usuario se registra

--- src/services/emailService.js
+++ src/services/emailService.js
@@ -5,3 +5,12 @@ const TEMPLATE_ID = import.meta.env.VITE_EMAILJS_TEMPLATE_ID;
 const PUBLIC_KEY = import.meta.env.VITE_EMAILJS_PUBLIC_KEY;
 
 emailjs.init(PUBLIC_KEY);
+
+export const sendWelcomeEmail = async (username, email) => {
+    const templateParams = {
+        to_name: username,
+        to_email: email,
+        from_name: "Wavv Music",
+        date: new Date().toLocaleDateString(),
+    }
+}
