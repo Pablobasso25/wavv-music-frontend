94537d9fbd44d2c53a5e09e464c08a0713f351da
Juan Ferreyra <juanitouu.f@gmail.com>
2026-02-11 18:41:59 -0300

    feat: agrega configuracion inicial de EmailJS junto con las variables env

---
+++ src/services/emailService.js
@@ -0,0 +1,7 @@
+import emailjs from "@emailjs/browser";
+
+const SERVICE_ID = import.meta.env.VITE_EMAILJS_SERVICE_ID;
+const TEMPLATE_ID = import.meta.env.VITE_EMAILJS_TEMPLATE_ID;
+const PUBLIC_KEY = import.meta.env.VITE_EMAILJS_PUBLIC_KEY;
+
+emailjs.init(PUBLIC_KEY);
