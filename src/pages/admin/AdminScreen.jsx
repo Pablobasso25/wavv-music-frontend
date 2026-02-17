4ea21ecb94787ed59dc84f24c623971b279f46ea
Juan Ferreyra <juanitouu.f@gmail.com>
2026-02-17 19:06:13 -0300

    [WM-22] feat: agrega imports de adminScreen

--- src/pages/admin/AdminScreen.jsx
+++ src/pages/admin/AdminScreen.jsx
@@ -1,11 +1,9 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { Container, Button, Nav, Card } from "react-bootstrap";
+import { defaultSongs, defaultUsers } from "../../data/dataDefault";
+import UsersTable from "./components/UsersTable";
+import SongsTable from "./components/SongsTable";
+import ArtistsTable from "./components/ArtistsTable";
+import SearchModal from "./components/SearchModal";
 
 const AdminScreen = () => {
-  return (
-    <div>
-      <p>Seccion de adminitrador</p>
-    </div>
-  )
-}
-
-export default AdminScreen
